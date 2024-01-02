f58c5ba7537505289d5fc37400e504f835e39725
SirRufo <oliver.muenzberg@gmail.de>
2024-01-02 14:29:30 +0100

    create a new image file and load it

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -1,3 +1,5 @@
+using System.IO;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Media.Imaging;
 
@@ -20,6 +22,17 @@ namespace WpfApp
         private void ResButton_Click( object sender, RoutedEventArgs e )
         {
             CustomImage.Source = new BitmapImage( new Uri( "/Images/res.png", UriKind.Relative ) );
+        }
+        private void NewButton_Click( object sender, RoutedEventArgs e )
+        {
+            var newpngfilename = Path.Combine( Path.GetDirectoryName( Assembly.GetExecutingAssembly().Location )!, "Images", "new.png" );
+            using ( var fileStream = new FileStream( newpngfilename, FileMode.Create ) )
+            {
+                var streaminfo = Application.GetResourceStream( new Uri( "/Images/res.png", UriKind.Relative ) );
+                streaminfo.Stream.CopyTo( fileStream );
+            }
+
+            CustomImage.Source = new BitmapImage( new Uri( newpngfilename, UriKind.Absolute ) );
         }
     }
 }
